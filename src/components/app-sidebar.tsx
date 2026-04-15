89d8eb03bc9ad0e232014089a51122798afbff2c
Jesus Maldonado <118919063+ElirPereza@users.noreply.github.com>
2026-04-15 11:19:33 -0500

    feat: add gradient border to sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -73,7 +73,11 @@ export function AppSidebar({ user, pendingCount = 0, ...props }: AppSidebarProps
   const pathname = usePathname()
 
   return (
-    <Sidebar collapsible="icon" className="top-(--header-height) h-[calc(100svh-var(--header-height))]!" {...props}>
+    <Sidebar 
+      collapsible="icon" 
+      className="top-(--header-height) h-[calc(100svh-var(--header-height))]! relative before:absolute before:right-0 before:top-0 before:bottom-0 before:w-[1px] before:bg-gradient-to-b before:from-[#6E2FE3] before:via-[#F31A7C] before:to-[#0CABF7] before:opacity-60"
+      {...props}
+    >
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
